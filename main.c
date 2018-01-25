ed75025e5ecadfc81f0ec7f18da8247474caf110
Dawid Sygocki <dawidsygocki1234@wp.pl>
2018-01-25 21:01:24 +0100

    Optymalizacja i naprawa błędów #2
    + poprawienie działania funkcji cod()
    + poprawienie błędu z nazwą pliku przy kodowaniu
    + dodanie znaku nowej linii co 6 "<E>" przy kodowaniu (dzięki za zauważenie tej prawidłowości, mysliwy112 xD)

--- main.c
+++ main.c
@@ -23,18 +23,24 @@ void cod(char *input, char *output, short var) {
     int optr, //output pointer
         iptr = 0, //input pointer
         len = strlen(input); //input length
-    short step = 0; //current cipher step
+    short step = 0, //current cipher step
+        ecnt = 0; //count of "<E>"
     bool crlf = true;
     sprintf(output, "{<C:%hu>}\n", var);
     optr = strlen(output);
     if (len > 0) {
+        if (!strstr(input, "\r\n")) crlf = false;
         while (iptr < len) {
             if (input[iptr] == '\r') {
+                output[optr] = '\0';
                 strcat(output, "<E>");
+                ecnt++;
                 iptr++;
                 optr += 3;
             } else if (input[iptr] == '\n') {
+                output[optr] = '\0';
                 strcat(output, "<E>");
+                ecnt++;
                 optr += 3;
             } else {
                 step++;
@@ -43,6 +49,14 @@ void cod(char *input, char *output, short var) {
                 if (step % 2) shift *= -1;
                 output[optr] = input[iptr] - shift;
                 optr++;
+            }
+            if (ecnt > 5) {
+                output[optr] = '\0';
+                /*strcat(output, crlf ? "\r\n" : "\n");
+                optr += crlf + 1;*/
+                strcat(output, crlf ? "\r\n" : "\n");
+                optr += crlf + 1;
+                ecnt = 0;
             }
             iptr++;
         }
@@ -69,21 +83,18 @@ void dec(char *input, char *output)
             if (input[iptr] == '<' && input[iptr + 1] == 'E' && input[iptr + 2] == '>') {
                 output[optr] = '\0';
                 strcat(output, crlf ? "\r\n" : "\n");
-                optr += crlf;
+                optr += crlf ? 2 : 1;
                 iptr += 2;
-            } else if (input[iptr] == '\r') {
-                output[optr] = '\0';
-                strcat(output, crlf ? "\r\n" : "\n");
-                optr++;
-            } else if (input[iptr] == '\n') output[optr] = '\n';
+            } else if (input[iptr] == '\r');
+            else if (input[iptr] == '\n');
             else {
                 step++;
                 if (step > mvmnt) step = 1;
                 short shift = step / 2 + step % 2;
                 if (step % 2) shift *= -1;
                 output[optr] = input[iptr] + shift;
+                optr++;
             }
-            optr++;
             iptr++;
         }
     }
@@ -106,14 +117,14 @@ int main(int argc, char **argv)
         return 0;
     } else if (argc == 3) { //operacja + plik
         fp = true;
-        if (argv[1][1] == 'd');
+        if (argv[1][1] == 'd');// decode = true;
         else if (argv[1][1] == 'e' || argv[1][1] == 'k') {
             decode = false;
         } else {
             print_help();
             return 0;
         }
-        src = fopen(argv[2], "r");
+        src = fopen(argv[2], "rb");
         if (!src) {
             fprintf(stderr, "Nieprawidlowa nazwa pliku: %s\n", argv[2]);
             return 0;
@@ -128,24 +139,26 @@ int main(int argc, char **argv)
             if (!decode) { //code
                 output = malloc(BUFSIZE * 1.5);
                 cod(input, output, 6);
-                char newname[strlen(argv[1]) + 5];
-                strcpy(newname, argv[1]);
+                char newname[strlen(argv[2]) + 5];
+                strcpy(newname, argv[2]);
                 strcat(newname, ".kod");
-                fnew = fopen(newname, "w");
+                fnew = fopen(newname, "wb");
                 len = strlen(output);
                 fwrite(output, len, 1, fnew);
                 fclose(fnew);
             } else { //decode
                 output = malloc(BUFSIZE);
                 dec(input, output);
-                char newname[strlen(argv[1]) + 5];
-                strcpy(newname, argv[1]);
+                char newname[strlen(argv[2]) + 5];
+                strcpy(newname, argv[2]);
                 strcat(newname, ".dek");
-                fnew = fopen(newname, "w");
+                fnew = fopen(newname, "wb");
                 len = strlen(output);
                 fwrite(output, len, 1, fnew);
                 fclose(fnew);
             }
+            free(input);
+            free(output);
         } else {
             fclose(src);
             fputs("Blad wejscia: plik zbyt duzy", stderr);
@@ -154,7 +167,7 @@ int main(int argc, char **argv)
     } else if (argc == 2) { //operacja ^ plik
         if (argv[1][0] != '/' && argv[1][0] != '-') {
             fp = true;
-            src = fopen(argv[1], "r");
+            src = fopen(argv[1], "rb");
             if (!src) {
                 fprintf(stderr, "Nieprawidlowa nazwa pliku: %s\n", argv[1]);
                 return 0;
@@ -172,7 +185,7 @@ int main(int argc, char **argv)
                     char newname[strlen(argv[1]) + 5];
                     strcpy(newname, argv[1]);
                     strcat(newname, ".kod");
-                    fnew = fopen(newname, "w");
+                    fnew = fopen(newname, "wb");
                     len = strlen(output);
                     fwrite(output, len, 1, fnew);
                     fclose(fnew);
@@ -182,11 +195,13 @@ int main(int argc, char **argv)
                     char newname[strlen(argv[1]) + 5];
                     strcpy(newname, argv[1]);
                     strcat(newname, ".dek");
-                    fnew = fopen(newname, "w");
+                    fnew = fopen(newname, "wb");
                     len = strlen(output);
                     fwrite(output, len, 1, fnew);
                     fclose(fnew);
                 }
+                free(input);
+                free(output);
             } else {
                 fclose(src);
                 fputs("Blad wejscia: plik zbyt duzy", stderr);
@@ -216,7 +231,9 @@ int main(int argc, char **argv)
                     dec(input, output);
                 }
                 puts(output);
+                free(output);
             }
+            free(input);
         }
     } else { //tryb interaktywny
         input = malloc(BUFSIZE);
@@ -233,9 +250,11 @@ int main(int argc, char **argv)
             fputs("Blad wejscia: brak wejscia\n", stderr);
         } else {
             output = malloc(BUFSIZE);
-            dec(input, output);
+            cod(input, output, 6);
             puts(output);
+            free(output);
         }
+        free(input);
     }
     return 0;
 }
