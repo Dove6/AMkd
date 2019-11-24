ad00f7c19d2afff87429b3857fa6c8c73098e157
Dove6 <dawidsygocki1234@wp.pl>
2019-11-24 15:09:56 +0100

    Added static functions for calculating output string size and comparing two settings structs
    Functions' implementation in progress

--- src/amkd.c
+++ src/amkd.c
@@ -13,6 +13,16 @@ static bool AMkd_compare_configs(AMkd_config left, AMkd_config right)
     }
 }
 
+static unsigned AMkd_calculate_size(const char *input_str, AMkd_config input_settings, AMkd_config output_settings)
+{
+    unsigned string_length = strlen(input_str) + 1;
+    if (input_settings.step_count != 0 && input_settings.step_count != 0) {
+        return string_length;
+    } else if (input_settings.step_count == 0) {
+        ;
+    }
+}
+
 AMkd_error AMkd_decode(const char *encoded_str, char **decoded_str, AMkd_config settings)
 {
     AMkd_error local_errno = AMKD_ERROR_NONE;
@@ -27,7 +37,13 @@ AMkd_error AMkd_decode(const char *encoded_str, char **decoded_str, AMkd_config 
             settings.letter = scanned_letter;
         }
     }
-    ;
+    for (int i = 1; i <= settings.step_count; i++) {
+        int movement = (i + 1) / 2;
+        if (i % 2 == 1) {
+            movement *= -1;
+        }
+
+    }
     return local_errno;
 }
 
@@ -40,7 +56,8 @@ AMkd_error AMkd_encode(const char *decoded_str, char **encoded_str, AMkd_config 
 AMkd_error AMkd_strip_header(char *encoded_str)
 {
     char trash_char;
-    int trash_int, string_length, characters_read;
+    int trash_int;
+    unsigned string_length, characters_read;
     string_length = strlen(encoded_str);
     if (sscanf(encoded_str, "{<%c:%d>}%n", &trash_char, &trash_int, &characters_read) != 2) {
         return AMKD_WARNING_MISSING_HEADER;
